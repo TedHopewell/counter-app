422e46dff76ad3dbff9ef13c918fc2887f4d6c67
TedHopewell <hopewellmaome@gmail.com>
2022-06-20 12:58:48 +0200

    counter app first update

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {decrement, increment} from './actions/index';
+
+import {useSelector, useDispatch} from 'react-redux'
+
+function App() {
+
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  return (
+    <div className="container">
+      <h1>Counter {counter}</h1>
+
+      <button className='btnIncrement' onClick={() => dispatch(increment())}> + </button>
+      <button className='btnDecrement' onClick={() => dispatch(decrement())}> - </button>
+    </div>
+  );
+}
+
+export default App;
